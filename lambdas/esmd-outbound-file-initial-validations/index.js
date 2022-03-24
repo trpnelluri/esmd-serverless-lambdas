58903c8ea3ea4752ed603a462420b6619be82859
SivakoteswaraRaoNell <sivakoteswararao.nelluri@cms.hhs.gov>
2022-03-24 15:40:18 -0400

    Updated the code to trigger the lambda from SQS queue when ever the file uploaded to S3.

--- lambdas/esmd-outbound-file-initial-validations/index.js
+++ lambdas/esmd-outbound-file-initial-validations/index.js
@@ -4,20 +4,23 @@ const LOBClassificationService = require('./lib/lob-classification');
 
 module.exports.handler = async function (event, context, callback) {
     console.log(`handler,Event received: ${JSON.stringify(event)}`);
-    const bucketName = event.Records[0].s3.bucket.name;
-    const fullFileName = event.Records[0].s3.object.key;
-    let fileNameArray = fullFileName.split('/');
-    //const fileName = fileNameArray[1].toUpperCase();
-    const fileName = fileNameArray[1];
-    const fileSize = event.Records[0].s3.object.size;
-    console.log(`-,handler,fullFileName: ${fullFileName} fileName: ${fileName} fileSize: ${fileSize}`);
-    context.callbackWaitsForEmptyEventLoop = false;
+    let lengthOfEvent = event.Records.length
+    console.log(`handler,event.Records[0]: ${lengthOfEvent}`);
     try{
+        const msgBody = JSON.parse(event.Records[0].body)
+        console.log(`handler,msgBody received: ${JSON.stringify(msgBody)}`);
+        const bucketName = msgBody.Records[0].s3.bucket.name;
+        const fullFileName = msgBody.Records[0].s3.object.key;
+        let fileNameArray = fullFileName.split('/');
+        const fileName = fileNameArray[1];
+        const fileSize = msgBody.Records[0].s3.object.size;
+        console.log(`-,handler,fullFileName: ${fullFileName} fileName: ${fileName} fileSize: ${fileSize}`);
+        context.callbackWaitsForEmptyEventLoop = false;
+        
         const pool = await PostgresPoolService.getInstance().connectToPostgresDB ()
-        const transID = 'ZMF0000276301EC'
         let lobClassificationService = LOBClassificationService.getInstance();
         let response = await lobClassificationService.classifyLOB(bucketName, fullFileName, fileName, fileSize, pool)
-        console.log(`${transID},handler,response: ${response}`);
+        console.log(`-,handler,response: ${response}`);
         return callback(null, response);
     } catch(err) {
         console.log(`-,handler,Error in catch: ${err}`);
